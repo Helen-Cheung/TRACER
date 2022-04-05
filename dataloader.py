38cbf4c747e5fae706d316cfadb8a1655f7c48d9
Helen-Cheung <62683546+Helen-Cheung@users.noreply.github.com>
2022-04-05 17:35:23 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -127,7 +127,7 @@ def get_train_augmentation(img_size, ver):
 def get_test_augmentation(img_size):
     transforms = albu.Compose([
         albu.Resize(img_size, img_size, always_apply=True),
-        albu.CLAHE(clip_limit=3.0, tile_grid_size=(24, 32), always_apply=True)
+        albu.CLAHE(clip_limit=3.0, tile_grid_size=(24, 32), always_apply=True),
         albu.Normalize([0.485, 0.456, 0.406],
                        [0.229, 0.224, 0.225]),
         ToTensorV2(),
